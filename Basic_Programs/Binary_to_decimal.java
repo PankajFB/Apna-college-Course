ea31fa80c0764fe33ff43487973c158354537751
Pankaj kumar <pk497243@gmail.com>
2023-02-23 20:10:49 +0530

    finally

--- Basic_Programs/Binary_to_decimal.java
+++ Basic_Programs/Binary_to_decimal.java
@@ -2,6 +2,20 @@ package com.company.Basic_Programs;
 
 public class Binary_to_decimal {
     public static void main(String[] args) {
-        System.out.println("");
+        biToDec(10111);
+    }
+
+    private static void biToDec(int binarynumber) {
+        int ourBinaryNumber = binarynumber;
+
+        int power = 0;
+        int dec = 0;
+       while (binarynumber > 0){
+           int lastDigit = binarynumber%10;
+           dec = dec + lastDigit*((int)Math.pow(2,power));
+           binarynumber = binarynumber/10;
+           power++;
+       }
+        System.out.println( +ourBinaryNumber + " in decimal is : " +dec);
     }
 }
