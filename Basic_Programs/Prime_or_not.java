e9f89163e57619ab5f83fd7055877b770fa2d998
Pankaj kumar <pk497243@gmail.com>
2023-02-23 22:52:51 +0530

    finally

--- Basic_Programs/Prime_or_not.java
+++ Basic_Programs/Prime_or_not.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class Prime_or_not {
     public static void main(String[] args) {
-        System.out.println(Prime_check(8));
+        System.out.println(Prime_check(6));
     }
 
     private static boolean Prime_check(int num) {
-        if(num == 2){
+
+
+        if(num == 2 || num == 1){
             return true;
         }
         for (int i = 2; i <= Math.sqrt((num)); i++) {
