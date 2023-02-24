82b81f6b7c6728b47c484c72200894d3710ac306
Pankaj kumar <pk497243@gmail.com>
2023-02-24 21:13:35 +0530

    some more basic questions and problem based on simple patterns

--- Arrays/Linear_Search.java
+++ Arrays/Linear_Search.java
@@ -1,5 +1,21 @@
 package com.company.Arrays;
 
 public class Linear_Search {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 10;
+        System.out.println(linearSearch(arr, target));
+    }
 
+    static int linearSearch(int[] arr, int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
